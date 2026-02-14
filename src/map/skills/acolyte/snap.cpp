e4368c5269458a7ab46925084b9926ebb260e2f7
popoza271 <apirat.kuaanan@gmail.com>
2026-02-14 11:45:37 +0700

    Asura Strike - Miss ! เมื่อถูกดูดลูกแก้วหรือถูกดิส 2

--- src/map/skills/acolyte/snap.cpp
+++ src/map/skills/acolyte/snap.cpp
@@ -15,6 +15,14 @@ SkillSnap::SkillSnap() : SkillImpl(MO_BODYRELOCATION) {
 void SkillSnap::castendPos2(block_list* src, int32 x, int32 y, uint16 skill_lv, t_tick tick, int32& flag) const {
 	map_session_data* sd = BL_CAST(BL_PC, src);
 
+	// [Puppy] Body Relocation cannot be used under Ankle or Spider Web.
+	if (sd && ((sd->sc.getSCE(SC_ANKLE) && battle_config.block_body_relocation_ankle) ||
+		(sd->sc.getSCE(SC_SPIDERWEB) && battle_config.block_body_relocation_spiderweb)))
+	{
+		clif_skill_fail(*sd, getSkillId(), USESKILL_FAIL_LEVEL, 0);
+		return;
+	}
+
 	if (unit_movepos(src, x, y, 2, 1)) {
 #if PACKETVER >= 20111005
 		clif_snap(src, src->x, src->y);
